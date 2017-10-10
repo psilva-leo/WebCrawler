a5194db609f352baba5cfe74407bdbee93aece51
Leonardo Silva <psilva.leo@gmail.com>
2017-10-10 00:41:52 -0300

    Adding Recaptcha solver (not working)

--- WebCrawler/spiders/news_spider.py
+++ WebCrawler/spiders/news_spider.py
@@ -4,39 +4,56 @@ import scrapy
 import re
 import json
 from WebCrawler.items import WebcrawlerItem
+from WebCrawler.recaptcha import RecaptchaEngine
+import numpy as np
 
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
-    start_urls = ['http://g1.globo.com/politica/']
     pages_depth = 3 # Is overwritten at Spider start (main)
     pages_visited_number = 0
     train_file = ''
+    proxy_file = 'proxy_list.txt'
+
+    def start_requests(self):
+        urls = [
+            'http://g1.globo.com/politica/',
+            'https://www.google.com/recaptcha/api2/demo']
+        for url in urls:
+            request = scrapy.Request(url=url, callback=self.parse)
+            request.meta['proxy'] = self.get_proxy()
+            print(request)
+            yield request
 
     def parse(self, response):
         self.pages_visited_number += 1
-        posts = response.css('.post-item')
-        for post in posts:
-            # follow links to news pages
-            href = post.css('a.feed-post-link::attr(href)').extract_first()
+        recaptcha = RecaptchaEngine(self)
+        if recaptcha.has_captcha(response):
+            recaptcha.solve_captcha(response)
 
-            item = WebcrawlerItem()
-            item['url'] = href
-            item['title'] = post.css('p.feed-post-body-title::text').extract_first()
-            item['abstract'] = post.css('p.feed-post-body-resumo::text').extract_first() if len(post.css('p.feed-post-body-resumo::text')) > 0 else ''
 
-            if item['title'] in self.get_viseted_pages_title():
-                return
-
-            request = scrapy.Request(href, callback=self.parse_author)
-            request.meta['item'] = item
-            yield request
-
-        #  Next page
-        next_page = response.css('div.load-more a::attr(href)').extract_first()
-        if next_page is not None and self.pages_visited_number < self.pages_depth:
-            next_page = 'http://g1.globo.com/' + next_page
-            yield scrapy.Request(next_page, callback=self.parse)
+        # posts = response.css('.post-item')
+        # for post in posts:
+        #     # follow links to news pages
+        #     href = post.css('a.feed-post-link::attr(href)').extract_first()
+        #
+        #     item = WebcrawlerItem()
+        #     item['url'] = href
+        #     item['title'] = post.css('p.feed-post-body-title::text').extract_first()
+        #     item['abstract'] = post.css('p.feed-post-body-resumo::text').extract_first() if len(post.css('p.feed-post-body-resumo::text')) > 0 else ''
+        #
+        #     if item['title'] in self.get_viseted_pages_title():
+        #         return
+        #
+        #     request = scrapy.Request(href, callback=self.parse_author)
+        #     request.meta['item'] = item
+        #     yield request
+        #
+        # #  Next page
+        # next_page = response.css('div.load-more a::attr(href)').extract_first()
+        # if next_page is not None and self.pages_visited_number < self.pages_depth:
+        #     next_page = 'http://g1.globo.com/' + next_page
+        #     yield scrapy.Request(next_page, callback=self.parse)
 
 
     def parse_author(self, response):
@@ -58,3 +75,10 @@ class NewsSpider(scrapy.Spider):
         for line in content:
             line = json.loads(line)['title']
             yield line
+
+    def get_proxy(self):
+        content = open(self.proxy_file, 'r')
+        lines = content.readlines()
+        index = np.random.choice(len(lines))
+        content.close()
+        return lines[index]
